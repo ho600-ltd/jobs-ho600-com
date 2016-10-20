754645be631c38de2cce6fd40d55b9a2cc7bf5e2
hoamon <hoamon+h97progamer@ho600.com>
2016-10-20 15:25:14 +0800

    consider two sites; redirect after waiting for 5 seconds.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -5,8 +5,9 @@ BUILD_URL=$2
 BUILD_NUMBER=$3
 REVISION=${4:0:4}
 BUILD_TIME=`date +%Y-%m-%dT%H:%M:%S%z`
+DIR_NAME=$5
 
-cat << EOF > $WORKSPACE/jobs-ho600-com/__version__.json
+cat << EOF > $WORKSPACE/${DIR_NAME}-ho600-com/__version__.json
 {
     "BUILD_URL": "`echo ${BUILD_URL} | sed 's/https\?:\/\/[^\/]\+\//http:\/\/build.dev.null\//'`",
     "BUILD_NUMBER": "${BUILD_NUMBER}",
@@ -15,9 +16,9 @@ cat << EOF > $WORKSPACE/jobs-ho600-com/__version__.json
 }
 EOF
 
-sed "s/\${BUILD_NUMBER}/${BUILD_NUMBER}/g" $WORKSPACE/jobs-ho600-com/index.html > $WORKSPACE/jobs-ho600-com/_index.html
-sed "s/\${REVISION}/${REVISION}/g" $WORKSPACE/jobs-ho600-com/_index.html > $WORKSPACE/jobs-ho600-com/__index.html
-sed "s/\${BUILD_TIME}/${BUILD_TIME}/g" $WORKSPACE/jobs-ho600-com/__index.html > $WORKSPACE/jobs-ho600-com/___index.html
-mv $WORKSPACE/jobs-ho600-com/___index.html $WORKSPACE/jobs-ho600-com/index.html
-rm -rf $WORKSPACE/jobs-ho600-com/*_index.html
-aws --profile s3-jobs-ho600-com-full-access s3 sync $WORKSPACE/jobs-ho600-com s3://jobs.ho600.com
+sed "s/\${BUILD_NUMBER}/${BUILD_NUMBER}/g" $WORKSPACE/${DIR_NAME}-ho600-com/index.html > $WORKSPACE/${DIR_NAME}-ho600-com/_index.html
+sed "s/\${REVISION}/${REVISION}/g" $WORKSPACE/${DIR_NAME}-ho600-com/_index.html > $WORKSPACE/${DIR_NAME}-ho600-com/__index.html
+sed "s/\${BUILD_TIME}/${BUILD_TIME}/g" $WORKSPACE/${DIR_NAME}-ho600-com/__index.html > $WORKSPACE/${DIR_NAME}-ho600-com/___index.html
+mv $WORKSPACE/${DIR_NAME}-ho600-com/___index.html $WORKSPACE/${DIR_NAME}-ho600-com/index.html
+rm -rf $WORKSPACE/${DIR_NAME}-ho600-com/*_index.html
+aws --profile s3-jobs-ho600-com-full-access s3 sync $WORKSPACE/${DIR_NAME}-ho600-com s3://${DIR_NAME}.ho600.com
